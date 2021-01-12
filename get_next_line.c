5537915a6e3c9951675e4c746e459bb4ef25b8d6
daumtto53 <daumtto53@gmail.com>
2021-01-12 22:59:01 +0900

    getline == 0 : when (read >=0) && meets EOF

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mchun <mchun@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/11 22:31:16 by mchun             #+#    #+#             */
-/*   Updated: 2021/01/12 21:36:58 by mchun            ###   ########.fr       */
+/*   Updated: 2021/01/12 22:56:44 by mchun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,6 @@ int					get_next_line(int fd, char **line)
 	char			read_buff[BUFFER_SIZE];
 
 	free(*line);
-	*line = NULL;
 	tb_node =  find_tb(&tb_head, fd);
 	index = -2;
 	//while ((index = find_tb_newline(tb_node, index + 2)) + 1 < 0)으로, <=을 줄 때는 segerror남.
@@ -36,15 +35,15 @@ int					get_next_line(int fd, char **line)
 		else
 			if (tb_dynamic_add(tb_node, read_buff, readlen) < 0)
 				return (-1);
-	}
-	if (index <= -2 && readlen == 0)	//이 때는 언제인가? while문에서 널을 찾지 못하고 readlen == 0에서 걸려서 break되었을 때
-	{
-		free(*line);
-		*line = NULL;
-		return (0);
-	}
+	}									//만약, while문에서 NULL을 못찾았는데, EOF를 만났다면?
+	// if (index <= -2 && readlen == 0)	//이 때는 언제인가? while문에서 널을 찾지 못하고 readlen == 0에서 걸려서 break되었을 때
+	// {
+	// 	free(*line);
+	// 	*line = NULL;
+	// 	return (0);
+	// }
 	if (strcpy_n_alloc(line, tb_node, 0, index) < 0)
 		return (-1);
 	move_tb_arr_n_cpy(tb_node, index);
-	return (1);
+	return ((index <= -2) ? (0) : (1));
 }
