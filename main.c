a772bae9ee70dc2c0db62156c4aa84ac40336d05
daumtto53 <daumtto53@gmail.com>
2021-01-31 14:06:53 +0900

    printer_type_xud: length part revisedD

--- main.c
+++ main.c
@@ -4,8 +4,8 @@
 
 int		main()
 {
-	printf("len : %d\n", ft_printf("%i, %d, %d, %d, %d, %s, %c, %d, %u, %x, %X\n",8, -12, 123456789, 0, -12345678, "abcdefghijklmnop", 'a', 97, 4294967284, 0xfffffff4, 0xFFFFFFF4));
-	printf("len : %d\n", printf("%i, %d, %d, %d, %d, %s, %c, %d, %lu, %x, %X\n",8, -12, 123456789, 0, -12345678, "abcdefghijklmnop", 'a', 97, 4294967284, 0xfffffff4, 0xFFFFFFF4));
+	printf("len : %d\n", ft_printf("%0*i, %0*d, %0*d, %0*d, %0*d, %0*d, %0*d, %0*d\n",8, -12, 123456789, 0, -12345678, 97, -2147483648, 2147483647));
+	printf("len : %d\n", printf("%0*i, %0*d, %0*d, %0*d, %0*d, %0*d, %0*d, %0*d\n",8, -12, 123456789, 0, -12345678, 97, -2147483648, 2147483647));
 	// printf(" --- Return : %d\n", ft_printf("%*.*d\n",4, 4, -12));
 	// printf(" --- Return : %d\n", printf("%*.*d\n",4, 4, -12));
 	// printf(" --- Return : %d\n", ft_printf("%d\n", -2147483648));
