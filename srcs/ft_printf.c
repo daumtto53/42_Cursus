c72f1e03f861d149d9cc5ecfa42ca0697c0cb14d
daumtto53 <daumtto53@gmail.com>
2021-02-05 21:59:29 +0900

    printf_ reverted

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: mchun <mchun@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/26 13:50:01 by mchun             #+#    #+#             */
-/*   Updated: 2021/02/05 21:48:24 by mchun            ###   ########.fr       */
+/*   Updated: 2021/01/31 18:17:09 by mchun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,90 +20,90 @@ void	parse_info_renew(t_info *info)
 	info->width = 0;
 }
 
-int		print_controller(t_info *info, va_list *ap, int *ft_errno)
+int		print_handler(t_info *info, va_list *ap, int *len)
 {
 	char	t;
-	int		len;
+	int		err;
 
-	len = 0;
 	t = info->type;
 	if (t == 'c')
-		len = printer_type_c(va_arg(*ap, int), info);
+		err = printer_type_c(info, ap, len);
 	else if (t == 's')
-		len = printer_type_s(va_arg(*ap, char *), info);
+		err = printer_type_s(info, ap, len);
 	else if (t == 'p')
-		len = printer_type_p(va_arg(*ap, void *), info, ft_errno);
+		err = printer_type_p(info, ap, len);
 	else if (t == 'x' || t == 'X')
-		len = printer_type_hex(va_arg(*ap, int), info, ft_errno);
+		err = printer_type_xud(info, ap, len);
 	else if (t == 'd' || t == 'i')
-		len = printer_type_int(va_arg(*ap, int), info, ft_errno);
+		err = printer_type_xud(info, ap, len);
 	else if (t == 'u')
-		len = printer_type_uint(va_arg(*ap, int), info, ft_errno);
+		err = printer_type_xud(info, ap, len);
 	else if (t == '%')
-		len = printer_type_c('%', info);
+		err = printer_type_perc(info, len);
 	else
-		*ft_errno = FT_ERR;
-	return (len);
+		return (-1);
+	return (err);
 }
 
-int		verify_p_info(t_info *info, int *ft_errno)
+int		handle_p_info(t_info *info)
 {
-	if (!ft_strchr("cspudxXi", info->type))
-		*ft_errno = FT_ERR;
-	if (info->width >= 2147483646 || info->prec >= 2147483646)
-		*ft_errno = FT_ERR;
-	if (!ft_strchr("diuxX", info->type) && \
-			(info->flag & F_ZERO & info->flag & F_PREC))
+	char	t;
+
+	t = info->type;
+	if (!(t == 'p' || t == 's' || t == 'c' || t == 'd' || t == 'u' || \
+			t == 'i' || t == 'x' || t == 'X' || t == '%'))
+		return (-1);
+	if (info->flag & F_ZERO && (t == 'p' || t == 's' || t == 'c'))
+		return (-1);
+	if (info->flag & F_ZERO && info->flag & F_LJUST)
 		info->flag &= (~F_ZERO);
-	if (info->prec < 0)
-	{
-		info->prec = 0;
+	if ((t == 'u' || t == 'd' || t == 'x' || t == 'X' || t == 'i') && \
+			(info->flag & F_ZERO) && (info->flag & F_PREC))
+		info->flag &= (~F_ZERO);
+	if (info->prec < 0 && info->flag & F_ZERO)
 		info->flag &= (~F_PREC);
-	}
-	if (info->width < 0)
-	{
-		info->width *= -1;
-		info->flag |= (F_LJUST);
-	}
-	return (FT_NORMAL);
+	if (info->width >= 2147483646 || info->prec >= 2147483646)
+		return (-1);
+	return (1);
 }
 
-int		print_until_delim(char **str, int *ft_errno)
+int		print_until_delim(const char *str, int i, int *len)
 {
-	const char	*str_unchanged;
-	const char	*str_changed;
-	int		len;
+	int		j;
 
-	str_changed = *str;
-	str_unchanged = *str;
-	while (*str_changed != '%' && *str_changed != '\0')
-		str_changed++;
-	if (write(1, str_unchanged, str_changed- str_unchanged) < 0)
-		*ft_errno = FT_ERR;
-	len = str_changed - str_unchanged;
-	*str = (char *)str_changed;
-	return (len);
+	j = i;
+	while (str[j] != '%' && str[j] != '\0')
+		j++;
+	if (write(1, str + i, j - i) < 0)
+		return (-1);
+	*len += (j - i);
+	return (j);
 }
 
 int		ft_printf(const char *str, ...)
 {
 	va_list			ap;
 	t_info			info;
+	int				i;
+	int				j;
 	int				len;
-	int				ft_errno;
 
-	ft_errno = FT_NORMAL;
+	if (str == NULL)
+		return (-1);
 	va_start(ap, str);
 	len = 0;
-	while (*str != '\0')
+	i = 0;
+	while (str[i] != '\0')
 	{
 		parse_info_renew(&info);
-		len += print_until_delim((char **)&str, &ft_errno);		//str : %에 위치함.
-		if (*str++ == '\0')
+		if ((j = print_until_delim(str, i, &len)) < 0)
+			return (-1);
+		if (str[j] == '\0')
 			return (len);
-		pf_parse((char **)&str, &info, &ap);
-		verify_p_info(&info, &ft_errno);
-		len += print_controller(&info, &ap, &ft_errno);
+		j += (pf_parse(str + j + 1, &info, &ap) + 1);
+		if (handle_p_info(&info) < 0 || print_handler(&info, &ap, &len) < 0)
+			return (-1);
+		i = j;
 	}
 	va_end(ap);
 	return (len);
--- srcs/pf_parse.c
+++ srcs/pf_parse.c
@@ -6,68 +6,101 @@
 /*   By: mchun <mchun@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/26 20:04:06 by mchun             #+#    #+#             */
-/*   Updated: 2021/02/05 21:45:33 by mchun            ###   ########.fr       */
+/*   Updated: 2021/02/01 17:50:45 by mchun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 
- int		is_valid_type(char c)
+int		parse_type(const char *s, t_info *info)
 {
-	return (ft_strchr("cspdixXu%%", c) != NULL);
-}
- int		is_valid_flag(char c)
-{
-	return (ft_strchr("0-*.", c) != NULL);
+	int		i;
+
+	i = 0;
+	info->type = s[i];
+	return (i + 1);
 }
 
-int				ft_abstract_atoi(char **s, va_list *ap)
+int		parse_precision(const char *s, t_info *info, va_list *ap)
 {
-	char	*p;
-	int			num;
+	int		i;
 
-	p = *s;
-	if (*p == '.')
-		p++;
-	if (*p == '*')
+	i = 0;
+	if (s[i] == '.')
 	{
-		p++;
-		return (va_arg(*ap, int));
+		info->flag |= F_PREC;
+		i++;
+		if (ft_isalpha(s[i]) || s[i] == '%')
+		{
+			info->prec = 0;
+			info->flag |= F_ONLY_DOT;
+		}
+		else if (s[i] == '*')
+		{
+			info->prec = va_arg(*ap, int);
+			if (info->prec < 0)
+				info->flag &= (~F_PREC);
+			i++;
+		}
+		else if (ft_isdigit(s[i]))
+			info->prec = ft_atoi(s + i);
+		while (ft_isdigit(s[i]))
+			i++;
 	}
-	else
-		num = ft_atoi(p);
-	while (ft_isdigit(*p))
-		p++;
-	p--;
-	*s = p;
-	return (num);
+	return (i);
 }
 
-int		pf_parse(char **s, t_info *info, va_list *ap)
+int		parse_width(const char *s, t_info *info, va_list *ap)
 {
-	char *p;
+	int		i;
 
-	p = *s;
-	while((is_valid_type(*p) != 0) && (is_valid_flag(*p) || ft_isdigit(*p)))
+	i = 0;
+	if (s[i] == '*')
 	{
-		if (*p == '0')
-			info->flag |= F_ZERO;
-		else if (*p == '-')
+		info->width = va_arg(*ap, int);
+		if (info->width < 0)
+		{
 			info->flag |= F_LJUST;
-		else if (*p == '*' || ft_isdigit(*p))
-		{
-			info->flag |= F_WIDTH;
-			info->width = ft_abstract_atoi(&p, ap);
+			info->flag &= (~F_ZERO);
+			info->width *= -1;
 		}
-		else if (*p == '.')
-		{
-			info->flag |= F_PREC;
-			info->prec = ft_abstract_atoi(&p, ap);
-		}
-		p++;
+		info->flag |= F_WIDTH;
+		i++;
 	}
-	if (is_valid_type(*p))
-		info->type = *p;
-	*s = p + 1;
-	return (FT_NORMAL);
+	else if (ft_isdigit(s[i]))
+	{
+		info->flag |= F_WIDTH;
+		info->width = ft_atoi(s + i);
+		while (ft_isdigit(s[i]))
+			i++;
+	}
+	return (i);
+}
+
+int		parse_flag(const char *s, t_info *info)
+{
+	int		i;
+
+	i = 0;
+	while (s[i] == '0' || s[i] == '-')
+	{
+		if (s[i] == '0')
+			info->flag |= F_ZERO;
+		else
+			info->flag |= F_LJUST;
+		i++;
+	}
+	return (i);
+}
+
+int		pf_parse(const char *s, t_info *info, va_list *ap)
+{
+	int		i;
+
+	i = 0;
+	i += parse_flag(s + i, info);
+	i += parse_width(s + i, info, ap);
+	i += parse_precision(s + i, info, ap);
+	i += parse_type(s + i, info);
+	return (i);
 }
