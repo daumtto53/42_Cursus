083ad7b42b0bf8408e9d4d1c3e7a37ebbec58955
daumtto53 <daumtto53@gmail.com>
2021-02-05 21:19:10 +0900

    /srcs modified, /ver1 newed, refactoring ver_2

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: mchun <mchun@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/26 13:50:01 by mchun             #+#    #+#             */
-/*   Updated: 2021/02/05 21:17:12 by mchun            ###   ########.fr       */
+/*   Updated: 2021/02/05 21:19:04 by mchun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ int		print_controller(t_info *info, va_list *ap, int *ft_errno)
 	char	t;
 	int		len;
 
+	len = 0;
 	t = info->type;
 	if (t == 'c')
 		len = printer_type_c(va_arg(*ap, int), info);
