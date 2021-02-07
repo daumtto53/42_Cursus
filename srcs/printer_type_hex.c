175a84421e91e8e4c9e51093901c0bf33247b76e
daumtto53 <daumtto53@gmail.com>
2021-02-07 19:03:43 +0900

    flag_# + implemented, with otype, dtype... norm not qualified

--- srcs/printer_type_hex.c
+++ srcs/printer_type_hex.c
@@ -6,7 +6,7 @@
 /*   By: mchun <mchun@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/05 18:01:06 by mchun             #+#    #+#             */
-/*   Updated: 2021/02/07 19:02:37 by mchun            ###   ########.fr       */
+/*   Updated: 2021/02/07 19:03:39 by mchun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ static int		hex_zero(unsigned long long num, t_info *i)
 
 	padd_len = -1;
 	ox_len = (i->flag & F_POUND) ? 2 : 0;
-	if (i->width > ft_digitlen_ubase(num, 16))
+	if (i->width > ft_digitlen_ubase(num, 16) + ox_len)
 		padd_len = (i->width - (ft_digitlen_ubase(num, 16)) + ox_len);
 	while (padd_len-- > 0)
 		ft_putchar_fd('0', 1);
