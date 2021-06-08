5eb13d1ae83ebcb59f1eb1a677593c45d86a5a86
Minsoo Chun <mchun@c3r2s7.42seoul.kr>
2021-06-08 21:54:04 +0900

    modified:   srcs/main.c 	modified:   srcs/redirection.c

--- srcs/redirection.c
+++ srcs/redirection.c
@@ -6,20 +6,34 @@
 /*   By: mchun <mchun@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/07 20:46:27 by mchun             #+#    #+#             */
-/*   Updated: 2021/06/08 14:45:37 by mchun            ###   ########.fr       */
+/*   Updated: 2021/06/08 21:33:11 by mchun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pipex.h"
 
-int		redirect_in()
+int		redirect_in(t_parsed **parsed)
 {
+	int		fd;
 
+	fd = open(parsed[CHILD_INDEX]->file, O_RDWR | O_CREAT, 0644);
+	if (fd < 0)
+		return (-1);
+	dup2(fd, STDIN_FILENO);
+	close(fd);
+	return (1);
 }
 
-int		redirect_out()
+int		redirect_out(t_parsed **parsed)
 {
+	int		fd;
 
+	fd = open(parsed[PARENT_INDEX]->file, O_RDWR | O_CREAT, 0644);
+	if (fd < 0)
+		return (-1);
+	dup2(fd, STDOUT_FILENO);
+	close(fd);
+	return (1);
 }
 
 int		connect_pipe()
