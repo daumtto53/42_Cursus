799cb5cb59cc78cabd33ae7ae3e811d59e8abff0
Minsoo Chun <mchun@c7r3s1.42seoul.kr>
2021-06-15 21:59:17 +0900

    deleted merged_files

--- main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "includes/ft_printf.h"
-
-int		main()
-{
-	printf("%d\n\n",ft_printf("%0*.*d\n", 3, 2, 8));
-	printf("%d\n\n",ft_printf("%d\n", 3123));
-}
--- test/vatest.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int		main()
-{
-	printf("[%#.x]\n", 0);
-	printf("[%#08x]\n", 30);
-}
