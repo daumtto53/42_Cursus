fdef2888571b506d9f0b53fcfa0f0f5d92eb5307
Minsoo Chun <mchun@c2r6s8.42seoul.kr>
2021-06-22 20:43:03 +0900

    branch test

--- philosophers/srcs/philo.c
+++ philosophers/srcs/philo.c
@@ -6,7 +6,7 @@
 /*   By: mchun <mchun@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/22 17:59:09 by mchun             #+#    #+#             */
-/*   Updated: 2021/06/22 20:41:21 by mchun            ###   ########.fr       */
+/*   Updated: 2021/06/22 20:42:25 by mchun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ int			main(int argc, char **argv)
 	i = 0;
 	while (i < attr.phil_num)
 	{
-		pthread_create(tid_arr + i, NULL, (void *)philo_thread, (void *)&attr);
+		pthread_create(tid_arr + i, NULL, (void *)philo_thread, (void *)&attr); sfsdf
 	}
 
 	i = 0;
