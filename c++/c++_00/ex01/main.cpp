6385bd494d0cd6c254ab21d94030e91c98fb09b0
cms <daumtto53@gmail.com>
2021-10-24 00:44:03 +0900

    C++

---
+++ c++/c++_00/ex01/main.cpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mchun <mchun@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/23 21:26:28 by mchun             #+#    #+#             */
+/*   Updated: 2021/10/24 00:34:06 by mchun            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "PhoneBook.hpp"
+
+int		main(void)
+{
+	PhoneBook	phonebook;
+	std::string	user_input;
+
+	while (1)
+	{
+		phonebook.prompt();
+		getline(std::cin, user_input);
+		std::cout << std::endl;
+		if (user_input.compare("ADD") == 0)
+			phonebook.add();
+		else if (user_input.compare("SEARCH") == 0)
+			phonebook.search();
+		else if (user_input.compare("EXIT") == 0)
+		{
+			phonebook.exit();
+			break;
+		}
+		else
+			phonebook.repeat();
+	}
+	return (0);
+}
---
+++ c++/cpp_practice/test_1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+
+int main()
+{
+	const char *str = "123123123123123";
+}
