a21e14e289e818a4e807fdff3e62d72e5c9c1c06
cms <daumtto53@gmail.com>
2021-10-26 13:33:36 +0900

    c++ 00

--- c++/c++_00/ex01/main.cpp
+++ c++/c++_00/ex01/main.cpp
@@ -6,13 +6,11 @@
 /*   By: mchun <mchun@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/23 21:26:28 by mchun             #+#    #+#             */
-/*   Updated: 2021/10/24 11:38:55 by mchun            ###   ########.fr       */
+/*   Updated: 2021/10/26 13:24:13 by mchun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PhoneBook.hpp"
-
-		#include <unistd.h>
 
 int		main(void)
 {
@@ -24,7 +22,6 @@ int		main(void)
 		phonebook.prompt();
 		getline(std::cin, user_input);
 		std::cout << "USER TEST : " << user_input << std::endl;
-			sleep(1);		//
 		if (user_input.compare("ADD") == 0)
 			phonebook.add();
 		else if (user_input.compare("SEARCH") == 0)
