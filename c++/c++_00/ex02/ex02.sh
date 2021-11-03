d01f3b5f47ee1693e9b25b0a9cca38f61ce2ea0d
Minsoo Chun <mchun@c7r3s1.42seoul.kr>
2021-11-03 20:44:26 +0900

    deleted c++(changed Directory)

--- c++/c++_00/ex02/ex02.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-clang++ -std=c++98 -pedantic-errors -Werror -Wextra -Wall -o a Account.cpp test.cpp;
-./a > log2
-
--- c++/cpp_practice/test_1.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <string>
-#include <cstring>
-
-int main()
-{
-	const char *str = "123123123123123";
-}
